7f9feb9d64e314e3348b56c573bb8307d6230f58
adrianradulescu <radulescu.adrian17@gmail.com>
2016-07-07 23:14:45 +0300

    Update top.v
    Am adaugat  cand sunt semnalele active si altceva despre ele.
    Apropo, ADDR e pe 23 de biti.

--- top.v
+++ top.v
@@ -6,21 +6,21 @@ module top(
     inout [15:0] DATA_INT,		// internal data bus (FPGA)
     inout [15:0] DATA_EXT,		// external data bus (mem)
     input RW,						// read/ write
-    input CS,						// chip select (RAM/FLASH)
-    output OE,						// output enable
-    output WE,						// write enable
-    output F_RST,					// FLASH reset
-    output F_CS,					// FLASH chip select
-    input F_STS,					// FLASH status
-    output R_ADV,					// RAM address valid
-    output R_CS,					// RAM chip select
+    input CS,						// chip select (RAM/FLASH)       0/1
+    output OE,						// output enable						0
+    output WE,						// write enable						0
+    output F_RST,					// FLASH reset							0
+    output F_CS,					// FLASH chip select    			dubios: e pe 3 sau doar pe 1 bit?
+    input F_STS,					// FLASH status						 ?
+    output R_ADV,					// RAM address valid					0
+    output R_CS,					// RAM chip select					1
     output R_CLK,					// RAM clock
-    output R_CRE,					// RAM control register enable
-    output R_LB,					// RAM lower byte
-    output R_UB,					// RAM upper byte
-    input R_WAIT,					// RAM wait
-    input [23:0] ADDR_INT,		// internal address bus (FPGA)
-    output [23:0] ADDR_EXT		// external address bus (mem)
+    output R_CRE,					// RAM control register enable	1
+    output R_LB,					// RAM lower byte						7:0
+    output R_UB,					// RAM upper byte						15:8
+    input R_WAIT,					// RAM wait								1
+    input [22:0] ADDR_INT,		// internal address bus (FPGA)
+    output [22:0] ADDR_EXT		// external address bus (mem)
     );
 
 
